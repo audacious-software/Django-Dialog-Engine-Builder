bc69085b3791b45327de7fc4380bc6e9a3db4d6d
Chris J. Karr <chris@audacious-software.com>
2021-09-25 13:33:55 -0700

    ID updates

--- apps.py
+++ apps.py
@@ -7,3 +7,4 @@ from django.apps import AppConfig
 class DjangoDialogEngineBuilderConfig(AppConfig):
     name = 'django_dialog_engine_builder'
     verbose_name = 'Dialog Engine: Builder'
+    default_auto_field = 'django.db.models.BigAutoField'
