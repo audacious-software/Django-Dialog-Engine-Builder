cf8584e666dcd36ab217dd5bc08b4a992cf8c6a5
Chris J. Karr <chris@audacious-software.com>
2023-06-26 20:38:21 +0000

    Code cleanup

--- static/builder-js/js/cards/echo.js
+++ static/builder-js/js/cards/echo.js
@@ -6,30 +6,30 @@ define(['material', 'cards/node', 'jquery'], function (mdc, Node) {
       body += '    <span class="mdc-notched-outline">'
       body += '      <span class="mdc-notched-outline__leading"></span>'
       body += '      <div class="mdc-notched-outline__notch">'
-      body += `        <label for="${ this.cardId }_message_value" class="mdc-floating-label">Message</label>`
+      body += `        <label for="${this.cardId}_message_value" class="mdc-floating-label">Message</label>`
       body += '      </div>'
       body += '      <span class="mdc-notched-outline__trailing"></span>'
       body += '    </span>'
       body += '    <span class="mdc-text-field__resizer">'
-      body += `      <textarea class="mdc-text-field__input" rows="4" style="width: 100%" id="${ this.cardId }_message_value"></textarea>`
+      body += `      <textarea class="mdc-text-field__input" rows="4" style="width: 100%" id="${this.cardId}_message_value"></textarea>`
       body += '    </span>'
       body += '  </label>'
       body += '<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-7 mdc-typography--caption" style="padding-top: 8px;">'
       body += '  The message above will be sent to the user and the system will proceed to the next card.'
       body += '</div>'
       body += '<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-5" style="padding-top: 8px; text-align: right;">'
-      body += `  <button class="mdc-icon-button" id="${ this.cardId }_next_edit">`
+      body += `  <button class="mdc-icon-button" id="${this.cardId}_next_edit">`
       body += '    <i class="material-icons mdc-icon-button__icon" aria-hidden="true">create</i>'
       body += '  </button>'
-      body += `  <button class="mdc-icon-button" id="${ this.cardId }_next_goto">`
+      body += `  <button class="mdc-icon-button" id="${this.cardId}_next_goto">`
       body += '    <i class="material-icons mdc-icon-button__icon" aria-hidden="true">navigate_next</i>'
       body += '  </button>'
       body += '</div>'
-      body += `<div class="mdc-dialog" role="alertdialog" aria-modal="true" id="${ this.cardId }-edit-dialog"  aria-labelledby="${ this.cardId }-dialog-title" aria-describedby="${ this.cardId }-dialog-content">`
+      body += `<div class="mdc-dialog" role="alertdialog" aria-modal="true" id="${this.cardId}-edit-dialog"  aria-labelledby="${this.cardId}-dialog-title" aria-describedby="${this.cardId}-dialog-content">`
       body += '  <div class="mdc-dialog__container">'
       body += '    <div class="mdc-dialog__surface">'
-      body += `      <h2 class="mdc-dialog__title" id="${ this.cardId }-dialog-title">Choose Destination</h2>`
-      body += `      <div class="mdc-dialog__content" id="${ this.cardId }-dialog-content"  style="padding: 0px;">`
+      body += `      <h2 class="mdc-dialog__title" id="${this.cardId}-dialog-title">Choose Destination</h2>`
+      body += `      <div class="mdc-dialog__content" id="${this.cardId}-dialog-content"  style="padding: 0px;">`
 
       body += this.dialog.chooseDestinationMenu(this.cardId)
       body += '      </div>'
@@ -47,19 +47,19 @@ define(['material', 'cards/node', 'jquery'], function (mdc, Node) {
     }
 
     viewBody () {
-      return `<div class="mdc-typography--body1" style="margin: 16px;">Sends: <em>${ this.definition.message }</em></div>`
+      return `<div class="mdc-typography--body1" style="margin: 16px;">Sends: <em>${this.definition.message}</em></div>`
     }
-    
-    updateCharacterCount() {
-        const value = $(`#${ this.cardId }_message_value`).val()
-        
-        const msgLength = value.length
-        
-        if (msgLength == 1) {
-        	$(`[for="${ this.cardId }_message_value"]`).html('Message (1 character)')
-        } else {
-        	$(`[for="${ this.cardId }_message_value"]`).html(`Message (${ msgLength } characters)`)
-        }
+
+    updateCharacterCount () {
+      const value = $(`#${this.cardId}_message_value`).val()
+
+      const msgLength = value.length
+
+      if (msgLength === 1) {
+        $(`[for="${this.cardId}_message_value"]`).html('Message (1 character)')
+      } else {
+        $(`[for="${this.cardId}_message_value"]`).html(`Message (${msgLength} characters)`)
+      }
     }
 
     initialize () {
@@ -67,14 +67,14 @@ define(['material', 'cards/node', 'jquery'], function (mdc, Node) {
 
       const me = this
 
-      const messageField = mdc.textField.MDCTextField.attachTo(document.getElementById(`${ this.cardId }_message_field`))
+      const messageField = mdc.textField.MDCTextField.attachTo(document.getElementById(`${this.cardId}_message_field`))
       messageField.value = this.definition.message
 
-      $(`#${ this.cardId }_message_value`).on('change keyup paste', function () {
+      $(`#${this.cardId}_message_value`).on('change keyup paste', function () {
         const value = $('#' + me.cardId + '_message_value').val()
 
         me.definition.message = value
-        
+
         me.updateCharacterCount()
 
         me.dialog.markChanged(me.id)
@@ -89,20 +89,20 @@ define(['material', 'cards/node', 'jquery'], function (mdc, Node) {
 
       const dialog = mdc.dialog.MDCDialog.attachTo(document.getElementById(me.cardId + '-edit-dialog'))
 
-      $(`#${ this.cardId }_next_edit`).on('click', function () {
+      $(`#${this.cardId}_next_edit`).on('click', function () {
         dialog.open()
       })
 
-      $(`#${ this.cardId }_next_goto`).on('click', function () {
+      $(`#${this.cardId}_next_goto`).on('click', function () {
         const destinationNodes = me.destinationNodes(me.dialog)
 
         for (let i = 0; i < destinationNodes.length; i++) {
           const destinationNode = destinationNodes[i]
 
           if (me.definition.next_id === destinationNode.id) {
-            $(`[data-node-id="${ destinationNode.id } "]`).css('background-color', '#ffffff')
+            $(`[data-node-id="${destinationNode.id} "]`).css('background-color', '#ffffff')
           } else {
-            $(`[data-node-id="${ destinationNode.id } "]`).css('background-color', '#e0e0e0')
+            $(`[data-node-id="${destinationNode.id} "]`).css('background-color', '#e0e0e0')
           }
         }
 
@@ -112,13 +112,13 @@ define(['material', 'cards/node', 'jquery'], function (mdc, Node) {
           const sourceNode = sourceNodes[i]
 
           if (me.definition.next_id === sourceNode.id) {
-            $(`[data-node-id="${ sourceNode.id } "]`).css('background-color', '#ffffff')
+            $(`[data-node-id="${sourceNode.id} "]`).css('background-color', '#ffffff')
           } else {
-            $(`[data-node-id="${ sourceNode.id } "]`).css('background-color', '#e0e0e0')
+            $(`[data-node-id="${sourceNode.id} "]`).css('background-color', '#e0e0e0')
           }
         }
       })
-      
+
       me.updateCharacterCount()
     }
 
