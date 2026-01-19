ebe3886d18f7a3785fad51bd2c0c8145438d8a1d
Chris J. Karr <chris@audacious-software.com>
2026-01-19 23:56:03 +0000

    Circle CI cleanup

--- circle_settings.py
+++ circle_settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE':   'django.db.backends.postgresql_psycopg2',
         'NAME':     'circle_test',
         'USER':     'root',
-        'PASSWORD': '',
+        'PASSWORD': '', # nosec
         'HOST':     'localhost',
         'PORT':     '',
     }
